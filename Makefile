a17fe0269a3628e1bf4b73f0dbbca4207c0b3ab8
Mohammad Iskandarani <M7md.k.isk@gmail.com>
2022-08-26 01:04:09 +0300

    Added arrow movement and edit functionality, multi platform support

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 compile:
-	gcc main.c -o main
+	gcc main.c -o main -ledit
--- main.c
+++ main.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  char* res;
+  size_t n;
+  ssize_t s = get_line(&res,&n,stdin);
+  res[s-1] = '\0';
+  return res;
+}
+void add_history(char* unused) {}
+
+#else
+
+#include <editline/readline.h>
+#include <editline/history.h>
+
+#endif
 static char* input = NULL;
 int main(int argc,char** argv){
-  printf("Hello, world!\n");
+  printf("HandyLisp V0.0.1\n");
+  printf("Press CTRL+C or type .exit to exit the program\n");
 
   do {
-    printf("lisp>");
-    size_t s;
-    ssize_t input_size = getline(&input, &s, stdin);
-    printf("%s",input);
+    input = readline("handy> ");
+    add_history(input);
+    printf("%s\n",input);
 
-  } while(strcmp(input,"exit\n") !=0) ;
+  } while(strcmp(input,".exit") !=0) ;
 }
-
 
